97013cad61ddf61c2760b6c56fdca539b97ce0d4
Ravi Joshi <ravipiyushjoshi@gmail.com>
2019-08-10 06:47:18 -0400

    updated apiRoutes with access of friends file and its content and homeRoutes with mechanism of routing on url

---
+++ routing/apiRoutes.js
@@ -0,0 +1,29 @@
+var friends = require("../app/data/friends");
+
+module.exports=function(app){
+
+
+
+app.get("/api/friends",function(req,res){
+    res.json(friends);
+});
+
+
+
+
+
+app.post("/api/friends", function(req, res) {
+  
+    
+ friends.push(req.body);
+        res.json(true);
+   
+
+  });
+
+
+
+}
+
+  
+ 
