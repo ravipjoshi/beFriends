801a4cd8526cacee0b058e927b6e109832ecd810
Ravi Joshi <ravipiyushjoshi@gmail.com>
2019-09-13 04:05:43 -0400

    updated files with the new html and few css in it

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,70 +1,59 @@
 var friends = 
 [
     {
-    "name":"Tej",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
+        "name":"Sherlock Holmes",
+        "photo":"https://vignette.wikia.nocookie.net/sherlock2010/images/3/3a/403SherlockWatchingMarysVideo.jpg/revision/latest/scale-to-width-down/439?cb=20180209025453",
+        "scores":[2,2,2,2,2]
     },
     {
-        "name":"Raj",
+        "name":"John Watson",
+        "photo":"https://vignette.wikia.nocookie.net/sherlock2010/images/d/dc/JohnWatsonPortal.png/revision/latest/scale-to-width-down/212?cb=20180616215850",
+        "scores":[3,3,3,3,3]
+          },
+    {
+        "name":"Mycroft Holmes",
         "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-          ]
+        "scores":[1,1,1,1,1]
+       },
+    {
+        "name":"Marry Watson",
+        "photo":"https://vignette.wikia.nocookie.net/sherlock2010/images/5/55/MaryProfile.png/revision/latest/scale-to-width-down/237?cb=20170117231457",
+        "scores":[3,3,3,3,1]
     },
     {
-        "name":"Jonas",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-          ]
+      "name":"Mrs.Hudson",
+      "photo":"https://vignette.wikia.nocookie.net/sherlock2010/images/0/02/Mrs_Hudson.jpg/revision/latest/scale-to-width-down/191?cb=20180616220129",
+      "scores":[1,3,1,3,1]
     },
     {
-        "name":"Eric",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-          ]
+      "name":"Molly Hooper",
+      "photo":"https://vignette.wikia.nocookie.net/sherlock2010/images/0/00/Molly-sherlock.jpg/revision/latest/scale-to-width-down/351?cb=20180616220255",
+      "scores":[1,1,2,2,3]
+    },
+
+    {
+      "name":"Greg Lestrade",
+      "photo":"https://vignette.wikia.nocookie.net/sherlock2010/images/2/2e/GregLestrade.jpg/revision/latest/scale-to-width-down/185?cb=20180616220359",
+      "scores":[3,1,2,1,2,]
+    },
+    {
+      "name":"Jim Moriarty",
+      "photo":"https://vignette.wikia.nocookie.net/bakerstreet/images/2/24/Jim_moriarty.png/revision/latest/scale-to-width-down/350?cb=20150601131646",
+      "scores":[3,1,2,2,3]
+    },
+    {
+      "name":"Irene Adler",
+      "photo":"https://vignette.wikia.nocookie.net/bakerstreet/images/8/87/Irene_Adler_-_A_Scandal_in_Belgravia.png/revision/latest/scale-to-width-down/350?cb=20150602012246",
+      "scores":[5,1,4,4,5]
+    },
+    {
+      "name":"Eurus Holmes",
+      "photo":"https://vignette.wikia.nocookie.net/bakerstreet/images/8/8e/Eurus-Holmes.png/revision/latest/scale-to-width-down/350?cb=20170116041044",
+      "scores":[5,1,4,4,5]
     },
     
     
     
-] 
+];
+
+module.exports = friends;
